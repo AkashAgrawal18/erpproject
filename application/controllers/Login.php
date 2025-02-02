efae5e9f9e06e0a0e5154e9acd3856c3b64eb203
digitalcoder-stack <akash.digitalshakha@gmail.com>
2025-02-02 20:35:42 +0530

    bugs fixed akash

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,37 +1,44 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Login extends CI_Controller {
+class Login extends CI_Controller
+{
 
- public function index(){  
-  $data['pagename'] = "Log-in";
+  public function index()
+  {
+    $data['pagename'] = "Log-in";
 
-  if($_SERVER["REQUEST_METHOD"] == "POST"){
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    $rules = array(
-      array('field'=>'login_email',   'label'=>'Email',   'rules'=>'trim|required'), 
-      array('field'=>'login_pass', 'label'=>'Password','rules'=>'trim|required')
-    );  
-    $this->form_validation->set_rules($rules);  
-    if ($this->form_validation->run() == FALSE) { }else{
+      $rules = array(
+        array('field' => 'login_email',   'label' => 'Email',   'rules' => 'trim|required'),
+        array('field' => 'login_pass', 'label' => 'Password', 'rules' => 'trim|required')
+      );
+      $this->form_validation->set_rules($rules);
+      if ($this->form_validation->run() == FALSE) {
+      } else {
+        $data = $this->Login_model->validate_user();
+        if ($data == 'not_found') {
+          $this->session->set_flashdata('status', '<div class="alert alert-danger"><i class="fa fa-warning"></i> Email-id not exists! Please try again </div>');
+        } else if ($data == 'pass_worng') {
+          $this->session->set_flashdata('status', '<div class="alert alert-danger"><i class="fa fa-warning"></i> Incorrect Password! Please try again </div>');
+        } else if(!empty($data)) {
+          $usrdata = array(
+            'is_user_in' => true,
+            'user_id' => $data[0]->m_emp_id,
+            'm_login_type' => $data[0]->m_login_type,
+            'designation' => $data[0]->m_emp_design,
+          );
+          // print_r($usrdata); die(); 
+          $this->session->set_userdata($usrdata);
+          redirect('Welcome');
+        }else {
+          $this->session->set_flashdata('status', '<div class="alert alert-danger"> <strong><i class="fa fa-warning"></i> &nbsp; Some Problem Occurred !...</strong> Please Try Again. </div>');
 
-      if($data = $this->Login_model->validate_user()){
-        $usrdata=array('is_user_in' => true,
-        'user_id' => $data[0]->m_emp_id,
-        'm_login_type' => $data[0]->m_login_type,
-        'designation' => $data[0]->m_emp_design,   
-      ); 
-			// print_r($usrdata); die(); 
-        $this->session->set_userdata($usrdata);
-        redirect('Welcome');
-      }else{ 
-        $this->session->set_flashdata('status','<div class="alert alert-danger"> <strong><i class="fa fa-warning"></i> &nbsp; Some Problem Occurred !...</strong> Please Try Again. </div>');
+        }
       }
-
     }
 
+    $this->load->view('login', $data);
   }
-
-  $this->load->view('login', $data); 
-}
 }
